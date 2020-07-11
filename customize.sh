cc7131e33582d9b48b04a1455488885e18ef40af
czf00852 <37551718+czf00852@users.noreply.github.com>
2020-07-11 20:23:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,8 +2,19 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Author: P3TERX
+# Blog: https://p3terx.com
 #=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.211/g' openwrt/package/base-files/files/bin/config_generate
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+
+#移除不用软件包
+rm -rf k3screenctrl package/lean
+#添加额外软件包
+src-git packages https://github.com/coolsnowwolf/packages
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git luci https://github.com/coolsnowwolf/luci
+src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07
+#src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07
+src-git helloworld https://github.com/fw876/helloworld
+#src-git small https://github.com/kenzok8/small
