8e24746574c56b40b0b2bc64c962608365aabc49
czf00852 <37551718+czf00852@users.noreply.github.com>
2020-07-11 20:41:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,10 +11,10 @@ sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generat
 #移除不用软件包
 rm -rf k3screenctrl package/lean
 #添加额外软件包
-src-git packages https://github.com/coolsnowwolf/packages
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git luci https://github.com/coolsnowwolf/luci
-src-git routing https://git.openwrt.org/feed/routing.git;openwrt-19.07
-#src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07
-src-git helloworld https://github.com/fw876/helloworld
-#src-git small https://github.com/kenzok8/small
+git clone packages https://github.com/coolsnowwolf/packages
+git clone kenzo https://github.com/kenzok8/openwrt-packages
+git clone luci https://github.com/coolsnowwolf/luci
+git clone routing https://git.openwrt.org/feed/routing.git;openwrt-19.07
+git clone telephony https://git.openwrt.org/feed/telephony.git;openwrt-19.07
+git clone helloworld https://github.com/fw876/helloworld
+git clone small https://github.com/kenzok8/small
